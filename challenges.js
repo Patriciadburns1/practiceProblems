841bc214d8b4c41b5908e4b5b08b222678a14270
dburns1 <patriciadburns@gmail.com>
2018-08-28 21:15:24 -0700

    first commit

--- challenges.js
+++ challenges.js
@@ -86,8 +86,39 @@ input: 'fruit'
 output: false
 */
 function checkFirstLetterCapitalized(word) {
+	for (var i =0; i < word.length-1; i++) {
+		if (word[i] == word[i].toUpperCase()) {
+			return true;
+		}
+		if (word[i] == word[i].toLowerCase()) {
+			return false;
+		}
+
+	}
+
 
 }
+
+console.log("this has something in uppercase,", checkFirstLetterCapitalized('Fruit'));
+
+function reverseString(str) {
+	// Step 1. Use the split() method to return a new array
+	var splitString = str.split(""); // var splitString = "hello".split("");
+	// ["h", "e", "l", "l", "o"]
+
+	// Step 2. Use the reverse() method to reverse the new created array
+	var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+	// ["o", "l", "l", "e", "h"]
+
+	// Step 3. Use the join() method to join all elements of the array into a string
+	var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+	// "olleh"
+
+	//Step 4. Return the reversed string
+	return joinArray; // "olleh"
+}
+
+console.log(reverseString("hello")); 
 
 /*
 CHECK OUTSIDE LETTERS
@@ -112,8 +143,8 @@ input: 'greetings earthlings'
 output: 'sgnilhtrae sgniteerg' 
 // */
 function reverseString(string) {
-	var newString = ''; 
-	for (var i = string.length - 1; i > 0 ; i--) {
+	var newString = '';
+	for (var i = string.length; i >= 0; i--) {
 		newString = newString + [string[i]]
 	}
 	return newString;
@@ -166,9 +197,51 @@ output: 'aahllmmorsw'
 input: 'tuba'
 output: 'abtu'
 */
-// function alphabetizeLetters(string) {
+function alphabetizeLetters(string) {
+	var newArray=[]; 
+	for (var i=0; i<string.length; i++){
+		newArray.push(string[i]); 
+	}
+	newArray.sort(); 
+	var newString = newArray.join(''); 
+	// newArray.join(); 
+	return newString; 
+}
 
-// }
+console.log(alphabetizeLetters("marshmallow")); 
 
+
+
+var first=["Dan", "Lisa", "Sam"];
+var last = [ "Smith","Burns", "Sale"];  
+
+function combineArrays( firstNames, lastNames){
+	var newArray =[]; 
+	  for(var i=x=0; i <firstNames.length; i++, x++){
+			  newArray.push({first: firstNames[i], last:lastNames[x]})
+		  
+	  }
+	return newArray; 
+  }
+  
+  console.log(combineArrays(first, last)); 
+  console.table(combineArrays(first, last)); 
+
+  function countLetters ( word ){
+    var vowels =0; 
+    var consonants =0; 
+    for (var x =0; x <word.length; x++){
+         if((word[x]) == "a" || (word[x]) == "e" || (word[x]) == "i" || (word[x]) == "o" || (word[x]) == "u" ) {
+             vowels = vowels + 1 ; 
+         }
+        else {
+        consonants = consonants + 1 ; 
+        }
+    }
+    return "vowels:" + vowels +  "consonants:" + consonants; 
+}
+
+console.log( countLetters("word")); 
+console.log( countLetters("elephant")); 
 
 
